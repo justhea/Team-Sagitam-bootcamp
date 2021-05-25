cb37bf7768108524aae9c195f7b44ee32077ec35
Sawcon deeznuts <jufranhundo@gmail.com>
2021-05-25 16:15:31 +0800

    Deleted comments

--- attorney.php
+++ attorney.php
@@ -12,32 +12,7 @@ include_once('font.php')?>
 		
 		include_once('header.php')
 		?>
-		<!--
-		<div id="site-content">
-			
-			<header class="site-header">
-				<div class="container">
-					<a href="index.php" id="branding">
-						<img src="images/logo.png" alt="Company Name" class="logo" width="100">
-						<div class="branding-copy">
-							<h1 class="site-title" style="font-size:60px;">Benedicto Law</h1>
-							<small class="site-description">Justice In Words</small>
-						</div>
-					</a>
-
-					<nav class="main-navigation">
-						<button type="button" class="menu-toggle"><i class="fa fa-bars"></i></button>
-						<ul class="menu">
-							<li class="menu-item"><a href="index.php">Home</a></li>
-						
-							<li class="menu-item current-menu-item"><a href="attorney.php">Attorney</a></li>
-							<li class="menu-item"><a href="service.php">Service</a></li>
-							<li class="menu-item"><a href="contact.php">Contact</a></li>
-						</ul>
-					</nav>
-					<nav class="mobile-navigation"></nav>
-				</div>
-			</header> site-header -->
+	
 
 			<main class="main-content">
 				
@@ -84,32 +59,7 @@ include_once('font.php')?>
 			<?php
 				include_once('footer.php')
 				?>
-<!--
-			<footer class="site-footer">
-				<div class="container">
-					<<div class="subscribe-form">
-						<form action="#">
-							<label for="#">
-								<span>Do you want to get news?</span>
-								<span>Join our news letter</span>
-							</label>
-							<div class="control">
-								<input type="text" placeholder="Enter your email to subscribe...">
-								<button type="submit"><img src="images/icon-envelope.png" alt=""></button>
-														</form>
-							</div>
-					</div>
-					<div class="social-links">
-						<a href="#"><i class="fa fa-facebook"></i></a>
-						<a href="#"><i class="fa fa-twitter"></i></a>
-						<a href="#"><i class="fa fa-google-plus"></i></a>
-						<a href="#"><i class="fa fa-pinterest"></i></a>
-					</div>
-					<div class="copy">
-						<p>Copyright 2014 Company name. Designed by Themeezy. All rights reserved.</p>
-					</div>
-				</div>
-			</footer>  .site-footer -->
+
 
 		</div> <!-- #site-content -->
 
