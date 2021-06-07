9baf4fcd632f1da9dd4ca9986842b13a4957dcd5
Sawcon deeznuts <jufranhundo@gmail.com>
2021-06-07 08:44:01 +0800

    Transfered content to laravel
    Note
    need to fix buttons

--- final_project_laravel/resources/views/index.blade.php
+++ final_project_laravel/resources/views/index.blade.php
@@ -6,7 +6,92 @@
 
 @section('content')
 
-    <h2>index page</h2>
+<main class="main-content">
+				<div class="hero">
+				<!--
+				<video autoplay loop class="video-background">
+					<source src="Lawyer.mp4" type="video/mp4">
+				
+				</video>--> <!-- tanggalin ko muna for the meantime-->
+					<div class="container">		
+							<div class="col-md-8">
+							</div>
+							<div class="col-md-4">
+								<div class="hero-content">
+								<h1 class="hero-title" style="font-size:40px;"><font face="Roboto">Justice In Words</font></h1>
+									<p class="redtext" style="font-size:20px;">BENEDICTO LAW, JUSTICE IS WHAT WE CREATE EVERYDAY. </p>
+									<p class="redtext" style="font-size:20px;">WITH DEEPER UNDERSTANDING, GET BETTER SOLUTIONS.</p>
+									<p class="redtext" style="font-size:20px;">SIGN UP TODAY, ITS FREE!</p>
+									<a href="about.php" class="button">Learn more</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div> <!-- .hero-slider -->
+				
+				<div id="ret" class="fullwidth-block" data-bg-color="#111113">
+					<div class="container">
+						<div class="rows">
+							<div class="col-md-8">
+								<h2 style="font-size:50px;">About The Benedicto Law</h2>
+								
+							</div>
+							<div class="col-md-4">
+								
+							</div>
+						</div> <!-- .row -->
+					</div> <!-- .container -->
+				</div> <!-- .fullwidth-block -->
+
+				<div class="fullwidth-block">
+					<div class="container">
+						<div class="row feature-list-section">
+							<div class="col-md-4">
+								<div class="feature">
+									<header>
+									
+										<div class="feature-title-copy">
+											<h2 class="feature-title">Criminal Things</h2>
+											<small class="feature-subtitle">Lorem ipsum dolor sit</small>
+										</div>
+									</header>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias doloremque, quis, eaque minus harum modi eius veritatis consequuntur expedita impedit ad, facilis. Asperiores assumenda aperiam atque, accusamus cupiditate vero sit!</p>
+									<a href="#" class="more-link">Read More</a>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="feature">
+									<header>
+										
+										<div class="feature-title-copy">
+											<h2 class="feature-title">Estate Planning</h2>
+											<small class="feature-subtitle">Lorem ipsum dolor sit</small>
+										</div>
+									</header>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias doloremque, quis, eaque minus harum modi eius veritatis consequuntur expedita impedit ad, facilis. Asperiores assumenda aperiam atque, accusamus cupiditate vero sit!</p>
+									<a href="#" class="more-link">Read More</a>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="feature">
+									<header>
+										
+										<div class="feature-title-copy">
+											<h2 class="feature-title">Employment Law</h2>
+											<small class="feature-subtitle">Lorem ipsum dolor sit</small>
+										</div>
+									</header>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias doloremque, quis, eaque minus harum modi eius veritatis consequuntur expedita impedit ad, facilis. Asperiores assumenda aperiam atque, accusamus cupiditate vero sit!</p>
+									<a href="#" class="more-link">Read More</a>
+								</div>
+							</div>
+						</div>
+						
+						
+					</div>
+				</div>
+
+			</main> <!-- .main-content -->
 
 @endsection
 
