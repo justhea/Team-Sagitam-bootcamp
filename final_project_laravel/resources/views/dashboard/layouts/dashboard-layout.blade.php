57274ee3d173199862d500674c9d99d964640400
Sawcon deeznuts <jufranhundo@gmail.com>
2021-06-07 15:05:36 +0800

    transferred css

--- final_project_laravel/resources/views/dashboard/layouts/dashboard-layout.blade.php
+++ final_project_laravel/resources/views/dashboard/layouts/dashboard-layout.blade.php
@@ -1,34 +1,30 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <!--Viewport for the things -->
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale=1">
-        
-        <title>{{ config('app.name') }}</title>
-        
-        <!--Icon for the lawfirm-->
-        <link rel="shortcut icon" href="{{ asset('download.png') }}" type="image/x-icon">
-        
-        <!-- Toast calendar css -->
-        <link rel="stylesheet" type="text/css" href="https://uicdn.toast.com/tui-calendar/latest/tui-calendar.css" />
-        <link rel="stylesheet" type="text/css" href="https://uicdn.toast.com/tui.date-picker/latest/tui-date-picker.css" />
-        <link rel="stylesheet" type="text/css" href="https://uicdn.toast.com/tui.time-picker/latest/tui-time-picker.css" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>{{$dashboardTitle ?? config('app.name') }}</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="{{ asset('css/dashboard/sb-admin-2.min.css') }}" rel="stylesheet">
 
         @stack('css')
     </head>
     <body>
         
-        <div style="width: 80%; margin: auto auto;">
+ 
             @yield('content')
-        </div>
-        
-        <script src="https://uicdn.toast.com/tui.code-snippet/v1.5.2/tui-code-snippet.min.js"></script>
-        <script src="https://uicdn.toast.com/tui.time-picker/latest/tui-time-picker.min.js"></script>
-        <script src="https://uicdn.toast.com/tui.date-picker/latest/tui-date-picker.min.js"></script>
-        <script src="https://uicdn.toast.com/tui-calendar/latest/tui-calendar.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+      
 
         @stack('script')
     </body>
