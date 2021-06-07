5204f388e3f685f18f4d7638bf58d5a2f0a68869
Sawcon deeznuts <jufranhundo@gmail.com>
2021-06-07 22:17:51 +0800

    Update index.blade.php

--- final_project_laravel/resources/views/dashboard/index.blade.php
+++ final_project_laravel/resources/views/dashboard/index.blade.php
@@ -6,14 +6,10 @@
 
 @section('content')
 
-    <div id="calendar" style="height: 800px;"></div>
+  
 
 @endsection
 
 @push('script')
-    <script>
-        var cal = new tui.Calendar('#calendar', {
-            defaultView: 'month' // monthly view option
-        });
-    </script>
+   
 @endpush
