e0594ca5cb23a5f2d30cf3a467c8436b5dfb3e9f
Sawcon deeznuts <jufranhundo@gmail.com>
2021-06-15 13:23:26 +0800

    Update RegisterController.php

--- final_project_laravel/app/Http/Controllers/RegisterController.php
+++ final_project_laravel/app/Http/Controllers/RegisterController.php
@@ -33,7 +33,7 @@ class RegisterController extends Controller
         'user_type' => 'admin',
         'bdate' => $request->get('bdate'),
         'contact_no' => $request->get('contact_no'),
-        'profile_image' => $request->get('profile_image')->store('http://localhost:8000/storage/images'),
+        'profile_image' => $request->file('profile_image') ? Storage::disk('public')->put('images',$request->profile_image) : 'http://localhost:8000/storage/images/default.jpg'
         //...
       ]);
   
